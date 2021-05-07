d8df89f35f8b6ddc7133af3d59c1488b907e65b8
Caue Moyano <cauegmoyano@gmail.com>
2021-05-07 16:54:16 -0300

    Implemented auto suggest search bar

---
+++ src/components/AutoSuggest.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import { teams } from "../teamsData";
+import { AiOutlineSearch, AiOutlineCloseCircle } from "react-icons/ai";
+import { Link } from "react-router-dom";
+
+const escapeRegexCharacters = (str) => {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+};
+const getSuggestions = (value) => {
+  const escapedValue = escapeRegexCharacters(value.trim());
+
+  if (escapedValue === "") {
+    return [];
+  }
+
+  const regex = new RegExp(escapedValue, "i");
+
+  return teams.filter((language) => regex.test(language.name));
+};
+
+const AutoSuggest = () => {
+  const [value, setValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [checkedValue, setCheckedValue] = useState([]);
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+  const onSuggestionsFetchRequested = (value) => {
+    setSuggestions(getSuggestions(value));
+  };
+  const onSuggestionsClearRequested = () => {
+    setSuggestions([]);
+  };
+  const selectTeam = (e) => {
+    setValue(e.target.innerText);
+    onSuggestionsClearRequested();
+  };
+  const checkValue = () => {
+    console.log(teams.some((team) => team.name === value));
+    return teams.some((team) => team.name === value);
+  };
+  useEffect(() => {
+    if (value.length === 0) return onSuggestionsClearRequested();
+    if (value.trim().length > 0) {
+      console.log(value);
+      onSuggestionsFetchRequested(value);
+    }
+  }, [value]);
+  return (
+    <div className="searchWrapper">
+      <input
+        type="text"
+        className="inputTeam"
+        value={value}
+        placeholder="Search a team"
+        onChange={(e) => handleChange(e)}
+      />
+      {suggestions.length === 0 ? (
+        ""
+      ) : suggestions[0].name === value ? (
+        ""
+      ) : (
+        <div className="teamSelectorWrapper">
+          {suggestions.map((team) => {
+            return (
+              <div
+                className="teamSelect"
+                key={team.id}
+                onClick={(e) => selectTeam(e)}
+              >
+                {team.name}
+              </div>
+            );
+          })}
+        </div>
+      )}
+      {value.length === 0 ? (
+        ""
+      ) : (
+        <div className="clearButton" onClick={() => setValue("")}>
+          <AiOutlineCloseCircle />
+        </div>
+      )}
+      <div className="searchButton">
+        <Link
+          to={
+            suggestions.length === 0
+              ? ""
+              : checkValue()
+              ? `/team/${suggestions[0].id}`
+              : ""
+          }
+        >
+          <AiOutlineSearch
+            style={{ padding: "5px", color: "white", fontSize: "3rem" }}
+          />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default AutoSuggest;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import DisplayCompetitionsLinks from "./DisplayCompetitionsLinks";
+import AutoSuggest from "./AutoSuggest";
 
 const Navbar = () => {
   return (
@@ -9,28 +10,7 @@ const Navbar = () => {
         <Link to="/" className="navbar-brand fs-2 text-light graduate-font">
           Football<span className="text-danger">Score</span>
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className="d-md-none collapse navbar-collapse"
-          id="navbarSupportedContent"
-        >
-          <ul className="d-md-none navbar-nav text-light me-auto mb-2">
-            <h5 className="graduate-font neon-effect text-center py-1">
-              Competitions
-            </h5>
-            <DisplayCompetitionsLinks />
-          </ul>
-        </div>
+        <AutoSuggest />
       </div>
     </nav>
   );
